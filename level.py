d22392f1f6560b4a8923c86af3bca7fe7b36b8fd
Augustin Kim <austinkim14@gmail.com>
2022-03-31 14:52:36 -0700

    Co-authored-by: Nate Trichanh <natetrichanhle@users.noreply.github.com>

--- level.py
+++ level.py
@@ -2,6 +2,7 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
+from debug import debug
 
 class Level:
     def __init__ (self):
@@ -10,7 +11,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         #sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         #sprite setup
@@ -24,9 +25,31 @@ class Level:
                 if col == 'x':
                     Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
                 if col == 'p':
-                    Player((x,y),[self.visible_sprites])
+                    self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
 
 
     def run (self):
         #update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        #general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self,player):
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(),key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
