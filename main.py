37397bc53e6d5751a5e8e1b2846ad5397c784f48
Nate Trichanh <natetrichanh@gmail.com>
2022-03-31 13:32:41 -0700

    level setup

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-HELLO WORLD
+import pygame, sys
+from settings import *
+from level import Level
 
-TESTING ur mom
+class Game:
+    def __init__ (self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Assassins Creed')
+        self.clock = pygame.time.Clock()
+
+        self.level = Level()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
